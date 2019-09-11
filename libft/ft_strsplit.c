7d0b64d5253e83dc9c22fe0dec4263f05ee5aad1
Ruslan Salakhetdinov <79852825377@yandex.ru>
2019-09-11 12:56:02 +0300

    need to finish ft_strsplit.c

--- libft/ft_strsplit.c
+++ libft/ft_strsplit.c
@@ -1,23 +1,45 @@
 #include "libft.h"
 
+static void ft_count_words(int *count, char *c)
+{
+    int     mark;
+
+    mark = 0;
+    while (*s)
+    {
+        if (*s != (*c) && mark != 1)
+        {
+            (*count)++;
+            mark++;
+        }
+        if (*s == (*c) && mark == 1)
+            mark--;
+        s++;
+    }
+
+}
+
+static char *ft_len_word(int *len, char *str)
+{
+
+}
+
 char        **ft_strsplit(char const *s, char c)
 {
     char    **arr;
     int     count;
-    int     mark;
+    int     i;
+    int     len;
 
-    mark = 0;
     count = 0;
-    while (*s)
+    ft_count_words(&count, &c);
+    arr = (char*)malloc(sizeof(*arr) * (count + 1));
+    i = -1;
+    while (++i < count)
     {
-        if (*s != c && mark != 1)
-        {
-            count++;
-            mark++;
-        }
-        if (*s == c && mark == 1)
-            mark--;
-        s++;
+        len = 0;
+        buf = ft_len_word(&len, buf)
     }
-    arr = (char*)malloc(sizeof(*arr) * 
+
 }
+
