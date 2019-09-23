cdb288c37e6e75143d52e954e08aca2bc84672bc
RuslanSalakhetdinov <cwheatgr@ox-l2.21-school.ru>
2019-09-23 20:05:01 +0300

    last update at 20:05 23.09.19

--- libft/ft_strsplit.c
+++ libft/ft_strsplit.c
@@ -5,60 +5,85 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: cwheatgr <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/09/11 16:41:26 by cwheatgr          #+#    #+#             */
-/*   Updated: 2019/09/13 16:55:53 by cwheatgr         ###   ########.fr       */
+/*   Created: 2019/09/23 17:47:14 by cwheatgr          #+#    #+#             */
+/*   Updated: 2019/09/23 19:08:28 by cwheatgr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static void	ft_count_words(int *count, char *c)
+static void	ft_count(char *s, char c, int *count)
 {
-	int		mark;
+	char	mark;
 
-	mark = 0;
+	mark = '0';
 	while (*s)
 	{
-		if (*s != (*c) && mark != 1)
+		if (*s != c && mark == '0')
 		{
 			(*count)++;
-			mark++;
+			mark = '1';
 		}
-		if (*s == (*c) && mark == 1)
-			mark--;
+		if (*s == c && mark == '1')
+			mark = '0';
 		s++;
 	}
 }
 
-static void	*ft_del_arr(char **arr, int index)
+static void	ft_del_arr(char **arr, int index)
 {
 	int i;
 
 	i = -1;
-	while (++i < index)
+	while (++i < (index - 1))
 		free(arr[i]);
+	free(arr);
 }
 
-static char	*ft_len_word(char *str, int *len)
+static void	ft_set_word(char **arr, char **s, char c, int index)
 {
-	
+	int		len;
+	char	*buf;
+
+	buf = (*s);
+	while (*buf == c)
+		buf++;
+	(*s) = buf;
+	len = 0;
+	while (*buf != c)
+	{
+		len++;
+		buf++;
+	}
+	arr[index] = (char*)malloc(sizeof(*(*arr)) * (len + 1));
+	if (!arr[index])
+		ft_del_arr(arr, index);
+	len = 0;
+	while (*(*s) != c)
+	{
+		arr[len][index] = *(*s);
+		len++;
+		(*s)++;
+	}
+	arr[len][index] = '\0';
 }
 
 char		**ft_strsplit(char const *s, char c)
 {
 	char	**arr;
 	int		count;
-	int		i;
-	int		len;
+	int		index;
 
 	count = 0;
-	ft_count_words(&count, &c);
-	arr = (char*)malloc(sizeof(*arr) * (count + 1));
-	i = -1;
-	while (++i < count)
+	ft_count(&(char*)*s, c, &count);
+	arr = (char**)malloc(sizeof(*arr) * (count + 1));
+	index = -1;
+	while (++index < count)
 	{
-		len = 0;
-		buf = ft_len_word(&len, buf)
+		ft_set_word(arr, &s, c, index);
+		if (!arr)
+			return (NULL);
 	}
+	arr[0][index] = '\0';
+	return (arr);
 }
-
