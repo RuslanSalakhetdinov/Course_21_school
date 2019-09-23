859db949c24ca5b17282c37285208c7ce050d203
RuslanSalakhetdinov <cwheatgr@ox-l2.21-school.ru>
2019-09-23 17:45:55 +0300

    last update at 17:45 23.09.19

--- libft/libft.h
+++ libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: cwheatgr <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/05 15:22:30 by cwheatgr          #+#    #+#             */
-/*   Updated: 2019/09/11 18:35:51 by cwheatgr         ###   ########.fr       */
+/*   Updated: 2019/09/20 20:24:57 by cwheatgr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -53,7 +53,35 @@ char			*ft_strnew(size_t size);
 char			**ft_strsplit(char const *s, char c);
 char			*ft_strsub(char const *s, unsigned int start, size_t len);
 char			*ft_strtrim(char const *s);
-void			*ft_memmove(void *dst, const char *src, size_t len);
-
+char			*ft_strchr(const char *s, int *c);
+int				ft_strcmp(const char *s1, const char *s2);
+int				ft_strncmp(const char *s1, const char *s2, size_t n);
+char			*ft_strstr(const char *haystack, const char *needle);
+char			*ft_strnstr(const char *haystack, \
+		const char *needle, size_t n);
+char			*ft_strrchr(const char *s, int *c);
+int				ft_atoi(const char *nptr);
+int				ft_isalnum(int c);
+int				ft_isalpha(int c);
+int				ft_isascii(int c);
+int				ft_isdigit(int c);
+int				ft_isprint(int c);
+char			*ft_itoa(int n);
+void			ft_lstadd(t_list **alst, t_list *new);
+void			ft_lstdel(t_list **alst, void (*del) (void *, size_t));
+void			ft_lstdelone(t_list **alst, void (*del) (void *, size_t));
+void			ft_lstiter(t_list *lst, void (*f) (t_list *elem));
+t_list			*ft_lstmap(t_list *lst, t_list *(*f) (t_list *elem));
+t_list			*ft_lstnew(void const *content, size_t content_size);
+void			ft_putchar(char c);
+void			ft_putchar_fd(char c, int fd);
+void			ft_putstr(const char *s);
+void			ft_putstr_fd(const char *s, int fd);
+void			ft_putnbr(int n);
+void			ft_putnbr_fd(int n, int fd);
+void			ft_putendl(const char *s);
+void			ft_putendl_fd(const char *s, int fd);
+int				ft_tolower(int c);
+int				ft_toupper(int c);
 
 #endif
